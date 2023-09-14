3bb543459004b39bd08421cec96ee6b0e7656fdf
rogerionegri <rogeriogalante@gmail.com>
2023-09-14 18:12:59 -0300

    update 14set23

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230914.175525';
+const cacheName = 'chirpy-20230914.181236';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
